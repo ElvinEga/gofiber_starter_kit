eeb7b49255abe5d103c21002364eda67f1f26e3d
Elvin Ega <proega3@gmail.com>
2025-08-25 12:50:29 +0300

    Add User and RefreshToken models
    This commit adds the User and RefreshToken models to the database
    migrations. The models are imported from the `models` package. Future
    models should be added to the `AutoMigrate` call.

--- database/migrations.go
+++ database/migrations.go
@@ -1,6 +1,16 @@
 package database
 
-// MigrateDB runs all database migrations
-func MigrateDB() {
+import (
+	"github.com/ElvinEga/gofiber_starter/models"
+)
 
+func MigrateDB() {
+	err := DB.AutoMigrate(
+		&models.User{},
+		&models.RefreshToken{},
+		// Add other models here
+	)
+	if err != nil {
+		panic("Failed to migrate database")
+	}
 }
