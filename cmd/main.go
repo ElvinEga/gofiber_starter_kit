c7eac913fd740d0a3c59f1b476b546f53fc5d5bf
Elvin Ega <proega3@gmail.com>
2025-08-25 12:17:54 +0300

    Add Swagger documentation
    Add Swagger UI to the application using the `fiber-swagger` library. The
    `/swagger/*` route now serves the Swagger UI. The previous static
    `/docs` route is commented out.

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,24 @@ import (
 
 	"github.com/ElvinEga/gofiber_starter/config"
 	"github.com/ElvinEga/gofiber_starter/database"
+	_ "github.com/ElvinEga/gofiber_starter/docs"
 	"github.com/ElvinEga/gofiber_starter/routes"
 	"github.com/garrettladley/fiberpaginate/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 )
 
+// @title Go Fiber Starter Kit API
+// @version 1.0
+// @description This is a sample Go Fiber API server.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email your-email@example.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8000
+// @BasePath /
 func main() {
 	config.InitConfig()
 	database.ConnectDB()
@@ -26,7 +38,8 @@ func main() {
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
 	}))
-	app.Static("/docs", "./docs")
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+	// app.Static("/docs", "./docs")
 	app.Use(fiberpaginate.New())
 	routes.SetupRoutes(app)
 
