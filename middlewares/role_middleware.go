c2b192c98d21dbd11eab0da6c1872c8bfae37854
Elvin Ambasa <elvinambasa@gmail.com>
2026-06-23 20:59:08 +0300

    Update role middleware for Fiber v3

--- middlewares/role_middleware.go
+++ middlewares/role_middleware.go
@@ -1,12 +1,12 @@
 package middlewares
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3"
 )
 
 // RequireRole ensures the user has the given role (e.g. “admin”)
 func RequireRole(role string) fiber.Handler {
-	return func(c *fiber.Ctx) error {
+	return func(c fiber.Ctx) error {
 		userRole := c.Locals("userRole") // set in your JWT middleware
 		if userRole != role {
 			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "forbidden"})
