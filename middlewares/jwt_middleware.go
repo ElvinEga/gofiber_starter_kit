4baa3f8b13f7f0e3380b02fe590ba1a210fb70ba
Elvin Ambasa <elvinambasa@gmail.com>
2026-06-23 21:01:10 +0300

    Update JWT middleware for Fiber v3 compatibility

--- middlewares/jwt_middleware.go
+++ middlewares/jwt_middleware.go
@@ -2,11 +2,11 @@ package middlewares
 
 import (
 	"github.com/ElvinEga/gofiber_starter/utils"
-	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v3"
 )
 
 func JWTProtected() fiber.Handler {
-	return func(c *fiber.Ctx) error {
+	return func(c fiber.Ctx) error {
 		userID, role, err := utils.VerifyJWT(c)
 		if err != nil {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
